553b2192ceeb562834fef2b21243bc1af5838621
jonathan <jonathan114311@gmail.com>
2026-05-21 14:30:14 -0500

    user story

---
+++ app.js
@@ -0,0 +1,12 @@
+const nameProduct = document.getElementById("name-product");
+const priceProduct = document.getElementById("price-product");
+const btnAdd = document.getElementById("btn-add");
+const formProduct = document.getElementById("form-product")
+
+formProduct.addEventListener("submit", (e) => {
+    e.preventDefault()
+    
+    const nameProductValue = nameProduct.value
+    const priceProductValue = priceProduct.value
+
+})
